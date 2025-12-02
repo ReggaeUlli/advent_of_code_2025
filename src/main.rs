f61623cb1f53dc9e17efba79fd05fe7df179b243
Sebastian Ulrich <sebastian.ulrich@ilt.fraunhofer.de>
2025-12-02 09:38:54 +0100

    finished day 01

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+mod day01;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let day: &String  = &args[1];
+    let day: u32 = day.parse().expect("Please provide a valid day number");
+
+    match day {
+        1 => { let _ = day01::day01(); }
+        // Add more days as you implement them:
+        // 2 => { let _ = day02::day02(); }
+        _ => {
+            println!("Day {day} is not implemented.");
+        }
+    }
+}
